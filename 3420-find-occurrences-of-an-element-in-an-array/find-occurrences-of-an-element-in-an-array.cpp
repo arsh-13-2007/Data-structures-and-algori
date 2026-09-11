d601dd55e28646d88402fc83ea07569673c925a1
arsh <arsh40896@gmail.com>
2026-09-11 21:57:32 +0530

    Time: 11 ms (52.69%) | Memory: 181.1 MB (88.86%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
         vector<int>ans(queries.size());
-        unordered_map<int , int>mp ; 
+        vector<int>pos ;
+        pos.push_back(-1) ;  
         int count = 0 ; 
         for( int i = 0 ; i < nums.size() ; i++){
             if(nums[i] == x){
                 count++ ; 
-                mp[count] = i ; 
+                pos.push_back(i) ; 
             }
         }
-
+        int n = pos.size() ; 
         for(int i = 0 ; i < queries.size() ;i++){
             if(queries[i] > count){
                 ans[i] = -1 ; 
                 continue ; 
-            }
-            ans[i] = mp[queries[i]] ; 
+            } 
+            ans[i] = pos[queries[i]] ; 
         }
     return ans ; 
     }
