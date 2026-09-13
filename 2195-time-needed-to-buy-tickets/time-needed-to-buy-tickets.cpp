f6a85cfcf2c875b148409e9f69a37d23f540d8a6
arsh <arsh40896@gmail.com>
2026-09-14 02:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.18%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>&nums, int k) {
+        queue<int>q ; 
+        int sum = 0  ; 
+        int num = nums[k] ; 
+        for( int i = 0 ; i <nums.size() ;i++ ){
+            if( i<= k  ){
+                sum = sum + min(nums[i] , num) ;
+            }
+           else{
+             sum = sum + min( nums[i] , num -1) ; 
+           }  
+     }
+    return sum ; 
+    }
+};
