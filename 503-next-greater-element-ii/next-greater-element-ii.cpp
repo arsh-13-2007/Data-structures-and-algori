1b8e260d7346f53f8f7ef5f988d4388b53282a70
arsh <arsh40896@gmail.com>
2026-09-14 00:47:23 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (14.52%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> s ; 
+        for(int i = nums.size() -1 ; i >=0 ; i--){
+            s.push(nums[i]) ; 
+        }
+
+        for(int i = nums.size()-1 ; i>=0 ; i--){
+
+            while(!s.empty() && nums[i] >= s.top()){
+                s.pop() ; 
+            }
+            if(s.empty()){
+                ans[i] = -1 ; 
+            }
+            else{
+                ans[i] = s.top() ;     
+            }
+            s.push(nums[i]) ;
+        }
+    return ans ; 
+    }
+};
